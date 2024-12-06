b3fa7b3e62f3a6cc21a8ee40f48ecad8a46271bd
Caio Almeida <caiofacchidev@gmail.com>
2024-12-06 16:42:03 -0300

    HTML functional through ejs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+//THIS SETS UP EJS AS RENDER ENGINE. HTML must be .ejs and MUST BE IN VIEWS FOLDER
+app.set('view engine','ejs');
+
 app.get("/",(req,res) => {
-    res.send("Route working");
+    res.render("index");
 });
 
 app.listen(8080, ()=>{
