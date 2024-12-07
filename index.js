aa91d9d589e93281cb8ec0b448fde0b1a2fb5eb2
Caio Almeida <caiofacchidev@gmail.com>
2024-12-07 05:07:53 -0300

    Repetition within HTML

--- index.js
+++ index.js
@@ -8,11 +8,30 @@ app.get("/:name/:lang",(req,res) => {
     var name = req.params.name;
     var lang = req.params.lang;
     var ShowMsg = true;
+
+    var items = [
+        {name: "Mountain Dew", price: 2.50},
+        {name: "Halo 3", price: 60.00},
+        {name: "Borderlands", price: 40.00},
+        {name: "Batman: Arkham Asylum", price: 59.95},
+        {name: "FortNite", price: 0.00},
+        {name: "Jedi - Fallen Order", price: 65.00},
+        {name: "Roller Coaster Tycoon", price: 15.00},
+        {name: "Sid Meyer's Locomotion", price: 5.00},
+        {name: "Desperados", price: 2.00},
+        {name: "Kingdom Hearts 1", price: 30.00},
+        {name: "Kingdom Hearts - Birth By Sleep", price: 20.00},
+        {name: "Kingdom Hearts 2", price: 40.00},
+        {name: "Kingdom Hearts - Dream Drop Distance", price: 9.00},
+        {name: "Kingdom Hearts 3", price: 60.00},
+
+    ]
     res.render("index",{
         name: name,
         lang: lang,
         company: "FacchiDev",
-        ShowMsg: ShowMsg
+        ShowMsg: ShowMsg,
+        items: items
     });
 });
 
