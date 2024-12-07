62d7a09a7084f525742600672f9051b51eaf1796
Caio Almeida <caiofacchidev@gmail.com>
2024-12-07 04:49:37 -0300

    Conditionals WITHIN the HTML. Remember to not use = after %, = is only used for variables!

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.set('view engine','ejs');
 app.get("/:name/:lang",(req,res) => {
     var name = req.params.name;
     var lang = req.params.lang;
+    var ShowMsg = false;
     res.render("index",{
         name: name,
         lang: lang,
-        company: "FacchiDev"
+        company: "FacchiDev",
+        ShowMsg: ShowMsg
     });
 });
 
